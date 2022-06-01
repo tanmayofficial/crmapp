b5f5222a0b0c52baa7530f177d5e26103b0d3a91
tanmayofficial <tanmaypaulofficial@gmail.com>
2022-06-01 20:13:13 +0530

    first commit crm app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Route, Routes, Router } from 'react-router-dom';
+import { Suspense } from 'react';
+import Login from './pages/Login';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Login />
+          </Suspense>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
